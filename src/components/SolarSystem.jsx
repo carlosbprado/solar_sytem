969947e8aea871a92e29970c1507320bee5f0307
carlosbprado <carlosbprado@outlook.com>
2022-08-18 14:57:20 -0300

    Requisito 2 feito

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system" />
+
+    );
+  }
+}
+
+export default SolarSystem;
