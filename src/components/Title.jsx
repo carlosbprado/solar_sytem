125641cb0e14b981efcaa9d28e9f09256d0d8d4e
carlosbprado <carlosbprado@outlook.com>
2022-08-18 15:14:53 -0300

    Requisito 3 feito

---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Title extends React.Component {
+  render() {
+    const element = <Title headline="" />;
+    return (
+      <h2>{element}</h2>
+    );
+  }
+}
+
+export default Title;
